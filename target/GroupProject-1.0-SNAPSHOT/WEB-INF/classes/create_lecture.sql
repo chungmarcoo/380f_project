1a411c8a9ab618eebe1327923620f6c18a6ef5ed
chung_marco <marco_97m@yahoo.com.hk>
2019-04-11 14:07:35 +0800

    ..

--- target/GroupProject-1.0-SNAPSHOT/WEB-INF/classes/create_lecture.sql
+++ target/GroupProject-1.0-SNAPSHOT/WEB-INF/classes/create_lecture.sql
@@ -1,41 +1,32 @@
-CREATE TABLE item (
-    itemId int,
-    customerName VARCHAR(50) NOT NULL,
-    subject VARCHAR(255) NOT NULL,
-    expectedPrice varchar(255) NOT NULL,
-    status varchar(20) not null,
-    noOfBids integer default 0,
-    description varchar(255),
-    winnerName varchar(50),
-    winnerPrice varchar(50),
-    PRIMARY KEY (itemId),
-    foreign key(customerName) references users(username)
+CREATE TABLE lecture (
+    lectureId int,
+    lectureName VARCHAR(50) NOT NULL,
+    PRIMARY KEY (lectureId),
+    foreign key(lectureName) references users(username)
 );
 
 create table attachments(
     attachmentId int,
     attachmentName varchar(50) not null,
     attachmentContent blob not null,
-    itemId int,
+    lectureId int,
     primary key(attachmentId),
-    foreign key(itemId) references item(itemId)
+    foreign key(lectureId) references lecture(lectureId)
 );
 
-create table bidPrices(
-    bidPriceId int,
-    bidder varchar(50) not null,
-    bidderPrice varchar(50) not null,
-    itemId int,
-    primary key(bidPriceId),
-    foreign key(itemId) references item(itemId)
+create table poll(
+    pollId int,
+    lectureId int,
+    primary key(pollId),
+    foreign key(lectureId) references lecture(lectureId)
 );
     
 create table comments(
     commentId int,
     cName varchar(50) not null,
     content varchar(255) not null,
-    itemId int,
+    lectureId int,
     primary key(commentId),
-    foreign key(itemId) references item(itemId)
+    foreign key(lectureId) references lecture(lectureId)
 );
 
